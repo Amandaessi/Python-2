e0b55aa6a879b3de660c7692be749ffd8ab7984a
Amanda <amanda141828@gmail.com>
2025-03-13 12:11:31 -0300

    Add files via upload

---
+++ cachorro.py
@@ -0,0 +1,36 @@
+class Cachorro:
+    def __int__(self, nome, raca, comida):
+        self.nome = nome
+        self.raca = raca
+        self.comida = comida
+        self.acordado = False 
+        self.feliz = False 
+        
+    def__str__(self):
+        return f"Nome: {self.nome}, Raça: {self.raca}, Comida: {self,comida}, Acordado: {self.acordado}, Feliz: {self.feliz}"
+    def acordar(self):
+        self.acordado = True
+        self.acordado = False
+        print("f"{self.nome} está brincando e está feliz")
+              
+    def acordar(self):
+        self.acordar -= 1
+        print{f"{self.nome} acordou")
+        
+    def dormir(self)
+        print(f"{self.nome} dormiu")
+        
+    def brincar(self)
+        self.feliz = True
+        print(f"{self.nome} está brincando feliz")
+        
+    def triste(self) 
+        self.triste = True 
+        print(f"{self.nome} está triste")
+        
+    def latir(self):
+        if self.latir is True:
+           print(f"self.nome} está latindo")
+    
+
+        
